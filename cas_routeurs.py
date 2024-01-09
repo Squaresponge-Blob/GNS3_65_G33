61302f2541365bd04be534493a1feea55b0bf6d7
Squaresponge-Blob <mailys.duthilleul@gmail.com>
2024-01-09 15:42:20 +0100

    ajout cas_routeurs.py et fonctions.py

---
+++ cas_routeurs.py
@@ -0,0 +1,22 @@
+"""
+vérifie la position du routeur et lance les configurations nécessaires
+"""
+
+from test_lecture_json import Routeur, liste_routeurs
+
+
+for r in liste_routeurs :
+    #Configurer les adresses physiques
+    
+
+    #Configurer les interfaces loopback 
+
+
+
+    #Le routeur est dans l'AS X
+
+    #Le routeur est dans l'AS Y
+
+    #Le routeur a un lien avec un routeur dans un autre AS (routeur de bord)
+
+    
---
+++ fonctions.py
@@ -0,0 +1,23 @@
+def Config_adresse(RX, adresse, int) :
+    """
+    Configuration de l'adresse (str) du routeur RX (str) sur l'interface int (str)
+    """
+def Config_loopback(RX,loopback) : 
+    """
+    Configuration de l'adresse loopback (str) du routeur RX (str)
+    """
+
+def RIP(RX) : 
+    """
+    Fait les configurations nécessaires pour lancer RIP sur le routeur RX
+    """
+def ID_BGP(RX, id) :
+    """
+    Config le routeur id pour BGP
+    """
+
+
+def ID_OSPF(RX,id) :
+     """
+    Config le routeur id pour OSPF
+    """
