a8187f7edde0db80b6039747a55b58098117330d
Squaresponge-Blob <mailys.duthilleul@gmail.com>
2024-01-09 15:40:37 +0100

    ajout fichiers cas_routeurs, fonctions

--- test_lecture_json.py
+++ test_lecture_json.py
@@ -1,15 +1,34 @@
 import json
 
-f = open("test.json","r")
+class Routeur : 
+    def __init__(self, nom, id, AS, protocole, loopback, voisins) :
+        self.nom = nom
+        self.id = id
+        self.AS = AS
+        self.protocole = protocole
+        self.loopback = loopback
+        self.voisins = voisins
+    def __str__(self) :
+        return f"{self.nom} d'ID : {self.id} appartient à l'AS {self.AS}"
+
+
+f = open("network_intent.json","r")
 content = f.read()
 obj=json.loads(content)
 
 routeurs = obj["Routeurs"]
 
+liste_routeurs = []
 for r in routeurs :
-    print(r["Nom"])
+    r = Routeur(r["Nom"],r["ID"],r["AS"],r["Protocole"],r["Loopback"],r["Voisins"])
+    liste_routeurs.append(r)
 
 
+
+
+
+    
+        
 
     
 
