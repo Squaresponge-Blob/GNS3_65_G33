71e8b06dd62ab6e8b15720a7d48168e96530fade
Tharathathal <thaliacharton@free.fr>
2024-01-09 16:09:49 +0100

    RIP

--- fonctions.py
+++ fonctions.py
@@ -22,6 +22,18 @@ def RIP(RX) :
     """
     Fait les configurations nécessaires pour lancer RIP sur le routeur RX
     """
+    tn.write(bytes("ipv6 router rip ripng\r",encoding= 'ascii'))
+    tn.write(bytes("redistribute connected\r",encoding= 'ascii'))
+    tn.write(bytes("exit\r",encoding= 'ascii'))
+
+def RIP_int(RX, int) :
+    """
+    Configure RIP sur l'interface int du routeur RX
+    """
+    tn.write(bytes("int "+int+"\r",encoding= 'ascii'))
+    tn.write(bytes("ipv6 rip ripng enable\r",encoding= 'ascii'))
+    tn.write(bytes("exit\r",encoding= 'ascii'))
+
 def ID_BGP(RX, id) :
     """
     Config le routeur id pour BGP
