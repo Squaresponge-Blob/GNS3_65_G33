629860b8545a1d14aa7221acd2cf75f559bc4afa
Squaresponge-Blob <mailys.duthilleul@gmail.com>
2024-01-12 10:28:33 +0100

    Modif cas_config

--- cas_config.py
+++ cas_config.py
@@ -25,50 +25,50 @@ for r in liste_routeurs :
 
     #Configurer les interfaces (en fonction du protocole)
     for v in r.voisins :
-        Config_interface(v["Int",v["Adresse"]]) #activer ipv6, donner une adresse
+        Config_interface(r.nom,v["Int",v["Adresse"]]) #activer ipv6, donner une adresse
         if r.AS == "1" :
-            Int_RIP() # ajoute la ligne pour activer RIP
+            Int_RIP(r.nom) # ajoute la ligne pour activer RIP
         if r.AS =="2":
-            Int_OSPF()
+            Int_OSPF(r.nom)
         
-        Config_interface("Loopback0", r.loopback)
+        Config_interface(r.nom,"Loopback0", r.loopback)
         if r.AS == "1" :
-            Int_RIP() # ajoute la ligne pour activer RIP
+            Int_RIP(r.nom) # ajoute la ligne pour activer RIP
         if r.AS =="2":
-            Int_OSPF()
+            Int_OSPF(r.nom)
 
     #OSPF interface passif
     #interface passif
     for v in r.voisins : 
         if v["AS"] != r.AS and r.AS == "2":
-            Config_int_passif(v["Int"])
+            Config_int_passif(r.nom,v["Int"])
 
     
     #BGP
     
-    Config_BGP() # bloc commun pour tous les routeurs (en eBGP et iBGP)
+    Config_BGP(r.nom) # bloc commun pour tous les routeurs (en eBGP et iBGP)
     for v in r.voisins :
 
         adresse_v = find_ad(r.nom, v["Nom"])   
-        Config_BGP(adresse_v,v["AS"]) # ligne neighbor [adresse_v] remote-as [AS]
+        Config_BGP(r.nom,adresse_v,v["AS"]) # ligne neighbor [adresse_v] remote-as [AS]
         
         #iBGP
         if v["AS"] == r.AS :
-            Config_iBGP(adresse_v) # ligne neighbor [adresse_v] update-source Loopback0
+            Config_iBGP(r.nom,adresse_v) # ligne neighbor [adresse_v] update-source Loopback0
     
-        Config_BGP2 #address-family ipv6
-        Config_BGP_activate(adresse_v) # la partie qui activate
+        Config_BGP2(r.nom) #address-family ipv6
+        Config_BGP_activate(r.nom,adresse_v) # la partie qui activate
 
 
     # configurer les protocoles (lignes à la fin)
     if r.protocole == "RIP" :
-        Config_RIP(r.id)
+        Config_RIP(r.nom,r.id)
     if r.protocole == "OSPF" :
-        Config_OSPF(r.id)
+        Config_OSPF(r.nom,r.id)
 
 
 
-    Config_fin() #trucs à la fin
+    Config_fin(r.nom) #trucs à la fin
 
 
     
