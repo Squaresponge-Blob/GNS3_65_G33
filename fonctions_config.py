963b2a9d6c79274111a55f89b6d5684d9705d49d
Squaresponge-Blob <mailys.duthilleul@gmail.com>
2024-01-19 14:10:41 +0100

    Rename for clarity

--- fonctions_config.py
+++ fonctions_config.py
@@ -31,26 +31,6 @@ lab.status
 # Verify the stats
 lab.stats
 
-def Great_Explosion_Murder_God_Dynamight():
-    routeur = 1
-    num_r = 1
-    list = []
-    adresses_routeur = {}
-
-    for node in lab.nodes: #récupère les id de chaque lien 
-        node.get()
-        for i in range(len(node.links)):
-            link = node.links[i]
-            if link.link_id not in list: 
-                list.append(link.link_id)
-                print(list)
-
-    for lien in list: #crée un dictionnaire associant les id des liens et leurs masques d'adresse ip respectives 
-        adresses_routeur[lien] = "2001:100:1:"+str(num_r)+"::"
-        num_r += 1    
-    print(adresses_routeur)
-    return adresses_routeur
-                                          
 
 def find_ad(a, b, liste_routeurs) : 
     """
@@ -198,7 +178,3 @@ def Ecrire_dans_fichier(config, nom):
         routeur += 1
     f = open(chemin,"wt")
     f.write(config)
-
-        
-           
-
