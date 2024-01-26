543d5f7aef6f8d25cbcd20f078a57cd011c01be4
Teo971 <frateo971@hotmail.com>
2024-01-26 17:44:05 +0100

    fonctions tn finies

--- fonctions_tn.py
+++ fonctions_tn.py
@@ -147,34 +147,49 @@ def tag_route_map(RX, status, tn):
         tn.write(bytes("route-map tag_client permit 10\r",encoding= 'ascii'))
         tn.write(bytes("set local-preference 200\r",encoding= 'ascii'))
         tn.write(bytes("set community 1:200\r",encoding= 'ascii'))
+        tn.write(bytes("exit\r",encoding= 'ascii'))
+
     if status == 'PEER':
+        tn.write(bytes("route-map filter permit 10\r",encoding= 'ascii'))
+        tn.write(bytes("match community-list client\r",encoding= 'ascii'))
+        tn.write(bytes("exit\r",encoding= 'ascii'))
         tn.write(bytes("route-map tag_peer permit 10\r",encoding= 'ascii'))
         tn.write(bytes("set local-preference 100\r",encoding= 'ascii'))
         tn.write(bytes("set community 1:100\r",encoding= 'ascii'))
+        tn.write(bytes("exit\r",encoding= 'ascii'))
+
     if status == 'PROV':
+        tn.write(bytes("route-map filter permit 10\r",encoding= 'ascii'))
+        tn.write(bytes("match community-list client\r",encoding= 'ascii'))
+        tn.write(bytes("exit\r",encoding= 'ascii'))
         tn.write(bytes("route-map tag_provider permit 10\r",encoding= 'ascii'))
         tn.write(bytes("set local-preference 50\r",encoding= 'ascii'))
         tn.write(bytes("set community 1:50\r",encoding= 'ascii'))
+        tn.write(bytes("exit\r",encoding= 'ascii'))
 
-def neighbor_route_map(RX, status, adresse,tn):
+def neighbor_route_map(RX, AS, status, adresse,tn):
     """
     Tag les neighbors route map sur le routeur RX
+    adresses sans /64
     """
     if status == 'CUST':
-        tn.write(bytes("router bgp 1\r",encoding= 'ascii'))
+        tn.write(bytes(f"router bgp "+ AS +"\r" ,encoding= 'ascii'))
         tn.write(bytes("address-family ipv6 unicast\r",encoding= 'ascii'))
         tn.write(bytes("neighbor "+ adresse +" send community\r",encoding= 'ascii'))
         tn.write(bytes("neighbor "+ adresse +" route-map filter out\r",encoding= 'ascii'))
         tn.write(bytes("neighbor "+ adresse +" route-map tag_client in\r",encoding= 'ascii'))
         tn.write(bytes("exit\r",encoding= 'ascii'))
+
     if status == 'PEER':
-        tn.write(bytes("router bgp 1\r",encoding= 'ascii'))
+        tn.write(bytes(f"router bgp "+ AS +"\r" ,encoding= 'ascii'))
         tn.write(bytes("neighbor "+ adresse +" send community\r",encoding= 'ascii'))
         tn.write(bytes("neighbor "+ adresse +" route-map filter out\r",encoding= 'ascii'))
         tn.write(bytes("neighbor "+ adresse +" route-map tag_peer in\r",encoding= 'ascii'))
         tn.write(bytes("exit\r",encoding= 'ascii'))
+
     if status == 'PROV':
-        tn.write(bytes("router bgp 1\r",encoding= 'ascii'))
-        tn.write()
+        tn.write(bytes(f"router bgp "+ AS +"\r" ,encoding= 'ascii'))
+        tn.write(bytes("neighbor "+ adresse +" send community\r",encoding= 'ascii'))
+        tn.write(bytes("neighbor "+ adresse +" route-map filter out\r",encoding= 'ascii'))
         tn.write(bytes("neighbor "+ adresse +" route-map tag_provider in\r",encoding= 'ascii'))
         tn.write(bytes("exit\r",encoding= 'ascii'))
